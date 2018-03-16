a282aa16e7e80c78013c8214c576d8e4160e726b
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-03-16 16:39:51 +0530

    Create 93.c

---
+++ 93.c
@@ -0,0 +1,10 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+int Kelvin,Celsius;
+cout<<"Enter temperature in Celsius ";
+cin>>Celsius;
+Kelvin=Celsius+273;
+cout<<" Temperature in Kelvin: "<<Kelvin;
+}
